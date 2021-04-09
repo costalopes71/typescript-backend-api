d4f238fb3010fa4f31d264f4b4deb211b7036485
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-04-08 23:22:42 -0300

    cria o modelo User

---
+++ src/model/user.model.ts
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+import * as mongoose from "mongoose";
+import { User } from "./user.interface";
+
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const userModel = mongoose.model<User & mongoose.Document>('User', userSchema);
+
+export default userModel;
